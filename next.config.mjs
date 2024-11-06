cd92173fe2e40a3da76f9360b666af9bb3d5461e
Naveen S Belandur <146941787+NaveenBelandurHelloMentor@users.noreply.github.com>
2024-11-06 18:33:38 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['s3-example-hello-mentor.s3.ap-south-1.amazonaws.com'],
+      },
+
+};
+
+export default nextConfig;
